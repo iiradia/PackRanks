fef1ba51765f3e3606dd977999f7efc1049ff3d5
iiradia <iiradia@ncsu.edu>
2020-05-07 16:24:07 -0400

    Created initial skeleton.

---
+++ project/static/index.js
@@ -0,0 +1,6 @@
+var gepornot = document.get_element_by_id("gepornot");
+var selected_type = e.options[e.selectedIndex].text;
+
+function gepOrNotSelect() {
+    $.get(selected_type)
+}
---
+++ run.sh
@@ -0,0 +1,3 @@
+##!/bin/bash
+
+python3 project/app.py
