91fc097c7e260e9d8008bab8d028a15b436d8d1a
iiradia <iiradia@ncsu.edu>
2020-05-09 16:21:21 -0400

    Added basic bootstrap template and modified frontend.

--- project/app.py
+++ project/app.py
@@ -10,20 +10,20 @@ def hello():
 
     return render_template("index.html")
 
-@app.route("/select", methods=["GET", "POST"])
+"""@app.route("/select", methods=["GET", "POST"])
 def gep_home():
-    """
+    
     Executes when go button is pressed from the homepage.
     Checks if gep or department was selected, and renders
     the appropriate html file.
-    """
+    
     #print (request.is_json)
     #print(request.data)
     #print(eval(request.data.decode("utf-8"))['gepordept'])
     if request.json['gepordept'] == "/gep":
         return render_template("gep.html")
     else:
-        return render_template("dept.html")
+        return render_template("dept.html")"""
 
 if __name__ == '__main__':
     app.run()
