e3262009ea35a369ceb275cb7986d618a451f676
Mathew Chanda <mvchanda@ncsu.edu>
2020-07-07 16:22:50 -0400

    Added updated tracking id for Google Analytics

--- reactjs/frontend/src/App.js
+++ reactjs/frontend/src/App.js
@@ -24,7 +24,7 @@ import {
 } from "react-router-dom";
 import ReactGA from "react-ga"
 
-ReactGA.initialize('UA-171857417-1');
+ReactGA.initialize('UA-171825338-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 
